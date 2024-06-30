fda83e4655582bd40054b5291fb91a64272df0b8
Noah <57148467+truuue@users.noreply.github.com>
2024-06-30 20:56:19 +0000

    [GLOBAL] Push all the start files

---
+++ web-fetcher/src/components/main/main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Web Fetcher</h1>
+        <p>Utility tool for everyone!</p>
+      </div>
+    );
+  }
+}
+
+export default Main;
