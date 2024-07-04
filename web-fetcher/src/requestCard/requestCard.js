280106379832400588c59a41349283fbc385694d
truuue <noahvernhet@gmail.com>
2024-07-04 02:04:25 +0200

    [FRONTEND] Add file type selection to URL input form

--- web-fetcher/src/requestCard/requestCard.js
+++ web-fetcher/src/requestCard/requestCard.js
@@ -4,10 +4,15 @@ import axios from "axios";
 class CardRequest extends React.Component {
   state = {
     url: "",
+    fileType: "jpg",
   };
 
   handleInputChange = (event) => {
     this.setState({ url: event.target.value });
+  };
+
+  handleFileTypeChange = (event) => {
+    this.setState({ fileType: event.target.value });
   };
 
   handleSubmit = async (event) => {
@@ -17,6 +22,7 @@ class CardRequest extends React.Component {
       .get(`${backendUrl}/fetch`, {
         params: {
           url: this.state.url,
+          fileType: this.state.fileType,
         },
       })
       .then((response) => {
@@ -34,13 +40,25 @@ class CardRequest extends React.Component {
         onSubmit={this.handleSubmit}
         className="bg-white w-72 h-80 rounded-md border-2 border-[#323232] shadow-[4px_4px_#323232] font-semibold flex flex-col justify-evenly items-center max-[430px]:-mb-48"
       >
-        <p>Your url input.</p>
+        <p>Your URL input.</p>
         <input
           onChange={this.handleInputChange}
           value={this.state.url}
           className="w-[200px] h-[40px] rounded-md border-2 border-[#323232] bg-white shadow-[2px_2px_#323232] text-[15px] font-semibold text-[#323232] px-[10px] py-[5px] outline-none placeholder-[#666] placeholder-opacity-80 focus:border-[#2d8cf0]"
           placeholder="URL"
         />
+        <p>Select file type:</p>
+        <select
+          value={this.state.fileType}
+          onChange={this.handleFileTypeChange}
+          className="w-[200px] h-[40px] rounded-md border-2 border-[#323232] bg-white shadow-[2px_2px_#323232] text-[15px] font-semibold text-[#323232] px-[10px] py-[5px] outline-none focus:border-[#2d8cf0]"
+        >
+          <option value="jpg">JPG</option>
+          <option value="jpeg">JPEG</option>
+          <option value="png">PNG</option>
+          <option value="gif">GIF</option>
+          <option value="svg">SVG</option>
+        </select>
         <button
           type="submit"
           className="p-2.5 bg-black text-white shadow-[1.5px_1.5px_#5C5C5C]"
