aab4f4e92a8975777bc574fe4e03b229db7c2801
truuue <noahvernhet@gmail.com>
2024-07-24 23:19:23 +0200

    Update main.js styling for Web Fetcher component

--- web-fetcher/src/components/main/main.js
+++ web-fetcher/src/components/main/main.js
@@ -34,14 +34,14 @@ class Main extends React.Component {
   render() {
     return (
       <div className="w-screen h-screen flex flex-col justify-evenly items-center max-[430px]:justify-self-auto">
-        <div className="w-full h-auto flex flex-col justify-center items-center max-[430px]:p-12">
+        <div className="w-full h-auto flex flex-col justify-center items-center max-[430px]:p-12 overflow-hidden">
           <img
             src="https://www.svgrepo.com/show/69948/rat-looking-right.svg"
             alt="ratIcon"
-            className="w-12 h-12 max-w-full max-h-full"
+            className="w-12 h-12 max-w-full max-h-full sm:w-16 sm:h-16 md:w-20 md:h-20 lg:w-24 lg:h-24"
           />
           <h1 className="text-6xl max-[430px]:text-3xl">Web Fetcher</h1>
-          <p className=" text-lg max-[430px]:text-sm">
+          <p className="text-lg max-[430px]:text-sm">
             Utility tool for everyone!
           </p>
         </div>
